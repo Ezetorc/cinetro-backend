ea8445d964bef30228683bf843100d4f8adcb674
Ezetorc <ezetorc@gmail.com>
2025-07-08 16:50:46 -0300

    Categories module re-factorized

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Delete, HttpStatus } from '@nestjs/common'
+import { Controller, Get, Post, Body, Delete, HttpStatus } from '@nestjs/common'
 import { CategoriesService } from './categories.service'
 import { CreateCategoryDto } from './dto/create-category.dto'
-import { UpdateCategoryDto } from './dto/update-category.dto'
 import { ApiDescription } from '../common/decorators/api-description.decorator'
 import { UsePolicy } from 'src/policy/decorators/use-policy.decorator'
 import { Name } from 'src/common/decorators/name.decorator'
@@ -32,15 +31,6 @@ export class CategoriesController {
   @ApiDescription('Returns the category with the id given', HttpStatus.OK)
   getById(@Name() name: string) {
     return this.categoriesService.getById(name)
-  }
-
-  @Patch(':id')
-  @UsePolicy('update', 'category')
-  @ApiName('Name of the category to update')
-  @ApiDescription('Category not found', HttpStatus.NOT_FOUND)
-  @ApiDescription('Returns the category updated', HttpStatus.OK)
-  update(@Name() name: string, @Body() updateDto: UpdateCategoryDto) {
-    return this.categoriesService.update(name, updateDto)
   }
 
   @Delete(':id')
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common'
 import { CreateCategoryDto } from './dto/create-category.dto'
-import { UpdateCategoryDto } from './dto/update-category.dto'
 import { PrismaService } from 'src/common/services/prisma.service'
 import { catchTo } from 'src/common/utilities/catch-to.utility'
 
@@ -9,7 +8,7 @@ export class CategoriesService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(data: CreateCategoryDto) {
-    return this.prismaService.category.create({ data })
+    return await catchTo(this.prismaService.category.create({ data }))
   }
 
   async getAll() {
@@ -18,10 +17,6 @@ export class CategoriesService {
 
   async getById(name: string) {
     return await this.prismaService.category.findUnique({ where: { name } })
-  }
-
-  async update(name: string, data: UpdateCategoryDto) {
-    return await catchTo(this.prismaService.category.update({ where: { name }, data }))
   }
 
   async delete(name: string) {
